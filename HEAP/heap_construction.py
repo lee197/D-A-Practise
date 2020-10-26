9237a1d3adee2e3c42e72104cda55475aaebb1bd
lee <leeqii197@gmail.com>
2020-10-26 16:58:51 +0000

    Fixed the heap construction

--- HEAP/heap_construction.py
+++ HEAP/heap_construction.py
@@ -5,42 +5,38 @@ class Heap:
     self.arr.append(-1)
     for i in range(len(arr)):
       self.arr.append(arr[i])
-
-    for item in range(1, int((len(self.arr) - 1 )/2)):
-      self.__shiftDown(item)
-
-  def print_heap(self):
-    print(self.arr)
+    self.count = len(arr)
+    for i in range(int(self.count/2), 0, -1):
+      self.__shiftDown(i)
 
   def isEmpty():
-    return len(self.arr) - 1 == 0
+    return self.count == 0
   
   def size():
-    return len(self.arr) - 1
+    return self.count
 
   def add(self, val):
     self.arr.append(val)
-    self.__shiftUp()
+    self.count += 1
+    self.__shiftUp(self.count)
 
   def remove(self):
     self.arr[1], self.arr[- 1] = self.arr[- 1], self.arr[1]
     val = self.arr.pop(-1)
+    self.count -= 1
     self.__shiftDown(1)
     return val
     
-  def __shiftUp(self):
-    k = len(self.arr) - 1
-    while k > 1 and self.arr[k/2] < self.arr[k]:
+  def __shiftUp(self, k):
+    while k > 1 and self.arr[int(k/2)] < self.arr[k]:
       self.arr[k/2], self.arr[k] = self.arr[k], self.arr[k/2]
       k /= 2
 
   def __shiftDown(self, k):
-    count = len(self.arr) - 1
-    print(k)
-  
-    while 2*k < count:
+    
+    while 2*k <= self.count:
       j = 2*k
-      if j + 1 <= count and self.arr[j+1] > self.arr[j]:
+      if j + 1 <= self.count and self.arr[j+1] > self.arr[j]:
         j += 1
       if self.arr[k] >= self.arr[j]:
         break
@@ -51,6 +47,14 @@ class Heap:
 import random
 
 randomlist = random.sample(range(0, 50), 10)
-print(randomlist)
+print("Initial radom: " + str(randomlist))
+
 heap = Heap(randomlist)
-heap.print_heap()
+print("After heapify: " + str(heap.arr))
+
+heap.add(20)
+print("After added: " + str(heap.arr))
+
+heap.remove()
+
+print("After removed: " + str(heap.arr))
